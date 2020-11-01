989269b4f9d2f1845dad880c22d6952debd6c05c
Ryan <ryanpivovar@gmail.com>
2020-11-01 14:56:18 -0500

    add commonjs rollup plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 
@@ -9,6 +10,7 @@ export default {
   },
   plugins: [
     nodeResolve(),
+    commonjs(),
     typescript(),
   ],
 };
