bbdad5cc6e6f0e87d611269e0c260468197d90c8
Ryan <ryanpivovar@gmail.com>
2020-11-01 14:43:14 -0500

    add rollup

---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+  input: 'src/index.tsx',
+  output: {
+    dir: 'dist',
+    format: 'esm',
+  },
+  plugins: [
+    nodeResolve(),
+    typescript(),
+  ],
+};
