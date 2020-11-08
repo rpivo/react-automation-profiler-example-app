51e3f76f98ce1f5311f00e9b720e69db3bfee7b4
Ryan <ryanpivovar@gmail.com>
2020-11-08 16:16:03 -0500

    add button to App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,11 +4,16 @@ import ProblemChild from './ProblemChild';
 import { AutomationProfiler } from 'react-automation-profiler';
 
 export default function() {
+  const [count, setCount] = React.useState(0);
+  function handleClick() {
+    setCount(count + 1);
+  }
+
   return (
     <AutomationProfiler id='p-app'>
       <div id='app'>
-        <ProblemChild />
-        <button>Click Me.</button>
+        <ProblemChild count={count} />
+        <button id='button' onClick={handleClick}>Click Me.</button>
       </div>
     </AutomationProfiler>
   );
