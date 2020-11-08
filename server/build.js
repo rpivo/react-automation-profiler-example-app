0a579c60d26ba3e14f013266794ee01569cf3981
Ryan <ryanpivovar@gmail.com>
2020-11-08 16:15:53 -0500

    update console log in build.js

--- server/build.js
+++ server/build.js
@@ -7,4 +7,4 @@ spawnSync('mkdir', ['dist']);
 spawnSync('rollup', ['-c']);
 spawnSync('cp', ['./src/index.html', './dist/index.html']);
 
-console.log(`finished build at ${new Date().toLocaleTimeString()}.`);
+console.log(`finished build at ${new Date().toLocaleTimeString()}.\n`);
