5ac41814f230b433059b02cf08b72b1c8132b42f
Ryan <ryanpivovar@gmail.com>
2020-11-09 18:10:27 -0500

    update example to use React.memo and React.useCallback

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,65 @@
 import React from 'react';
-import ProblemChild from './ProblemChild';
 
 import { AutomationProfiler } from 'react-automation-profiler';
 
-export default function() {
-  const [count, setCount] = React.useState(0);
-  function handleClick() {
-    setCount(count + 1);
-  }
+const MemoizedCountButton = React.memo(({ count, id, onClick }: {
+  count: number;
+  id: string;
+  onClick: () => void;
+}) =>
+  <AutomationProfiler id='p-memo-button'>
+    <button id={id} onClick={onClick}>{count}</button>
+  </AutomationProfiler>
+);
 
+const CountButton = ({ count, id, onClick }: {
+  count: number;
+  id: string;
+  onClick: () => void;
+}) =>
+  <AutomationProfiler id='p-button'>
+    <button id={id} onClick={onClick}>{count}</button>
+  </AutomationProfiler>;
+
+/* comment out CountButton above and comment in CountButton 
+below to see how React.memo() affects renders */
+
+// const CountButton = React.memo(({ count, id, onClick }: {
+//   count: number;
+//   id: string;
+//   onClick: () => void;
+// }) =>
+//   <AutomationProfiler id='p-button'>
+//     <button id={id} onClick={onClick}>{count}</button>
+//   </AutomationProfiler>
+// );
+
+function DualCounter() {
+  const [count1, setCount1] = React.useState(0);
+  const [count2, setCount2] = React.useState(0);
+
+  const callbackIncrement = React.useCallback(() => setCount1(count => count + 1), []);
+
+  const increment = () => setCount2(count => count + 1);
+
+  /* comment out increment above and comment in increment 
+  below to see how React.useCallback() affects renders */
+
+  // const increment = React.useCallback(() => setCount2(count => count + 1), []);
+
+  return (
+    <>
+      <MemoizedCountButton count={count1} onClick={callbackIncrement} id='memoized-button' />
+      <CountButton count={count2} onClick={increment} id='button' />
+    </>
+  )
+}
+
+export default function () {
   return (
     <AutomationProfiler id='p-app'>
       <div id='app'>
-        <ProblemChild count={count} />
-        <button id='button' onClick={handleClick}>Click Me.</button>
+        <DualCounter />
       </div>
     </AutomationProfiler>
   );
--- src/components/ProblemChild.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function ({ count }: { count: number }) {
-  return <div id='problem-child'>{count}</div>;
-}
