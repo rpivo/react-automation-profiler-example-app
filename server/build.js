b84c22b4ad6b5173ded1b242618ba413e11fc7ac
Ryan <ryanpivovar@gmail.com>
2021-01-14 10:48:00 -0500

    update build and serve scripts

--- server/build.js
+++ server/build.js
@@ -1,10 +1,14 @@
 import { spawnSync } from 'child_process';
 
+const processes = [
+  ['rm', ['-rf', 'dist']],
+  ['mkdir', ['dist']],
+  ['rollup', ['-c']],
+  ['cp', ['./src/index.html', './dist/index.html']],
+];
+
 console.log('preparing build...');
 
-spawnSync('rm', ['-rf', 'dist']);
-spawnSync('mkdir', ['dist']);
-spawnSync('rollup', ['-c']);
-spawnSync('cp', ['./src/index.html', './dist/index.html']);
+for (const process of processes) spawnSync(...process, { stdio: 'inherit' });
 
 console.log(`finished build at ${new Date().toLocaleTimeString()}.\n`);
